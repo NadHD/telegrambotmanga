de5710447eb854cceb97a1aa28f47a0323356d7e
Daniele Nicolazzo <s258453@studenti.polito.it>
2021-11-21 16:40:34 +0100

    Prova 5

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-/*const { Telegraf } = require('telegraf')
+const { Telegraf } = require('telegraf')
 const bot = new Telegraf('2110225858:AAGPjnDrQ3skov42uYLZ32RzsqCol5dXk-E')
 
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: ""
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+client.connect();
 
 bot.start((message) => {
 	return message.reply('Il bot è avviato')
 })
+
 bot.command('eur', context=> {
 	  msg=context.update.message
   importo=msg.text.split(' ')[1]
@@ -28,17 +28,5 @@ bot.command('usd', context=> {
   euro=importo/EXCHANGE
   context.reply(`${euro} EUR`)
 })
-bot.launch()*/
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+bot.launch()
