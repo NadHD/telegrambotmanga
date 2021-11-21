d74f714742031f59a72af9a2a59f975aef1c754e
Daniele Nicolazzo <s258453@studenti.polito.it>
2021-11-21 17:24:37 +0100

    Prova 9

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const bot = new Telegraf('2110225858:AAGPjnDrQ3skov42uYLZ32RzsqCol5dXk-E')
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+	user: 'sastjtgosyizfr',
+    password: 'dbf2113d05da3f4e6de9d1ce13575859971714b2cb6eaa3c8da4aa8cb469d8e5',
+    database: 'deksproc2dlgv', 
+    host: 'ec2-54-155-200-16.eu-west-1.compute.amazonaws.com',
+    port: 5432
 });
 
 client.connect();
@@ -24,3 +28,7 @@ bot.command('usd', context=> {
 })
 
 bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
